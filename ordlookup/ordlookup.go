c4e1a29bf189e3ae48040a5b2e8bbd1867af86e1
er28-0652 <33626923+er28-0652@users.noreply.github.com>
2018-06-09 19:23:56 +0900

    ADD: add ordlookup module

---
+++ ordlookup/ordlookup.go
@@ -0,0 +1,26 @@
+package ordlookup
+
+import (
+	"fmt"
+	"strings"
+)
+
+var OrdNames = map[string]map[uint64]string{
+	"ws2_32.dll":   WS2_32_ORD_NAMES,
+	"wsock32.dll":  WS2_32_ORD_NAMES,
+	"oleaut32.dll": OLEAUT_32_ORD_NAMES,
+}
+
+func OrdLookup(libname string, ord uint64, makeName bool) string {
+	names, ok := OrdNames[strings.ToLower(libname)]
+	if ok {
+		if name, ok := names[ord]; ok {
+			return name
+		}
+	}
+	if makeName {
+		return fmt.Sprintf("ord%d", ord)
+	}
+
+	return ""
+}
